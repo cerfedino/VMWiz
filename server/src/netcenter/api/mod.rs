e0ce1c980b28fb5edfce84b1e0233e0332fded4b
Jonas Maier <>
2024-07-13 18:22:32 +0200

    netcenter api stuff

--- server/src/netcenter/api/mod.rs
+++ server/src/netcenter/api/mod.rs
@@ -42,3 +42,47 @@ pub async fn free_ipv4(cfg: &NetCenter, subnet: &str) -> anyhow::Result<Vec<IpAd
 
     Ok(ips)
 }
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct NameToIp {
+    ip: IpAddr,
+    ip_subnet: IpAddr,
+    fqname: String,
+    // forward
+    // reverse
+    ttl: u32,
+    // dhcp
+    // ddns
+    isg_group: String,
+    // views
+}
+
+//<usedIps>
+//<usedIp>
+//<ip>192.33.91.50</ip>
+//<ipSubnet>192.33.91.0</ipSubnet>
+//<fqname>app.vsos.ethz.ch</fqname>
+//<forward>Y</forward>
+//<reverse>Y</reverse>
+//<ttl>3600</ttl>
+//<dhcp>N</dhcp>
+//<ddns>N</ddns>
+//<isgGroup>adm-soseth</isgGroup>
+//<lastDetection>2024-03-19 12:33</lastDetection>
+//<views>
+//<view>extern</view>
+//<view>intern</view>
+//</views>
+//</usedIp>
+//</usedIps>
+
+// known endpoints
+// GET /netcenter/rest/nameToIP/freeIps/v4/{subnet}
+// GET /netcenter/rest/nameToIP/usedIps/v4/{subnet}
+// GET /netcenter/rest/nameToIP/{ip}
+// GET /netcenter/rest/nameToIP/{ip}/{hostname}
+// GET /netcenter/rest/alias/{alias}
+// GET /netcenter/rest/alias/hostName/{hostname}
+// GET /netcenter/rest/user/listAllIsgs
+// GET /netcenter/rest/user/listIsgByUser
