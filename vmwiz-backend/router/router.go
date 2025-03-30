ebe05fbc5c18bbbeee1b55c5813f1bd949463d1b
Albert Cerfeda <cerfeda.albert@crfda.com>
2025-03-30 20:27:41 +0200

    Added VM deletion by name endpoint

--- vmwiz-backend/router/router.go
+++ vmwiz-backend/router/router.go
@@ -8,6 +8,7 @@ import (
 
 	"git.sos.ethz.ch/vsos/app.vsos.ethz.ch/vmwiz-backend/auth"
 	"git.sos.ethz.ch/vsos/app.vsos.ethz.ch/vmwiz-backend/form"
+	"git.sos.ethz.ch/vsos/app.vsos.ethz.ch/vmwiz-backend/netcenter"
 	"git.sos.ethz.ch/vsos/app.vsos.ethz.ch/vmwiz-backend/notifier"
 	"git.sos.ethz.ch/vsos/app.vsos.ethz.ch/vmwiz-backend/proxmox"
 	"git.sos.ethz.ch/vsos/app.vsos.ethz.ch/vmwiz-backend/storage"
@@ -126,6 +127,42 @@ func Router() *mux.Router {
 			return
 		}
 	}))
+
+	r.Methods("POST").Path("/api/vm/deleteByName").Subrouter().NewRoute().Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		type bodyS struct {
+			Name string `json:"vmName"`
+		}
+
+		var body bodyS
+		err := json.NewDecoder(r.Body).Decode(&body)
+		if err != nil {
+			log.Printf("Error decoding JSON: %v", err)
+			http.Error(w, "Invalid request payload", http.StatusBadRequest)
+			return
+		}
+
+		vms, err := proxmox.GetAllNodeVMsByName("comp-epyc-lee-3", body.Name)
+		if err != nil {
+			log.Printf("Error getting VM by name: %v", err)
+			http.Error(w, "Failed to get VM by name", http.StatusInternalServerError)
+			return
+		}
+		if len(*vms) == 0 {
+			log.Printf("No VM found with name: %s", body.Name)
+			http.Error(w, "No VM found with the given name", http.StatusNotFound)
+			return
+		}
+		vm := (*vms)[0]
+		err = proxmox.DeleteNodeVM("comp-epyc-lee-3.sos.ethz.ch", vm.Vmid, true, true, false)
+		if err != nil {
+			log.Printf("Error deleting VM: %v", err)
+			http.Error(w, "Failed to delete VM", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}))
+
 	r.Methods("POST").Path("/api/dns/deleteByHostname").Subrouter().NewRoute().Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		type bodyS struct {
 			Hostname string `json:"hostname"`
