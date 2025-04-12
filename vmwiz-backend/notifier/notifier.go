51f4167dee24604dcee43a3f115cb9bf3db28c93
Albert Cerfeda <cerfeda.albert@crfda.com>
2025-04-12 14:26:14 +0200

    Fixes + escaping text in triple back ticks

--- vmwiz-backend/notifier/notifier.go
+++ vmwiz-backend/notifier/notifier.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+
+	"git.sos.ethz.ch/vsos/app.vsos.ethz.ch/vmwiz-backend/storage"
 )
 
 var THREAD_TITLE = "VM Request Notifications"
 
-func UseNotifier(tags string, body string) error {
+func useNotifier(tags string, body string) error {
 
 	fmt.Println("[-] Sending notification to vmwiz-notifier")
 	v := url.Values{}
@@ -24,5 +26,24 @@ func UseNotifier(tags string, body string) error {
 }
 
 func SendTestNotification(body string) error {
-	return UseNotifier("test", body)
+	return useNotifier("test", body)
+}
+
+func NotifyVMRequest(req storage.SQLVMRequest) error {
+	return useNotifier("new_vmrequest", "```\n"+req.ToString()+"\n```")
+}
+
+func NotifyVMRequestStatusChanged(req storage.SQLVMRequest, additional_text string) error {
+	switch req.RequestStatus {
+	case storage.STATUS_ACCEPTED:
+		return useNotifier("vmrequest_accepted", fmt.Sprintf("Request %v approved ! %v", req.ID, additional_text))
+	case storage.STATUS_REJECTED:
+		return useNotifier("vmrequest_rejected", fmt.Sprintf("Request %v denied ! %v", req.ID, additional_text))
+	}
+
+	return nil
+}
+
+func NotifyVMCreationUpdate(msg string) error {
+	return useNotifier("vmcreation_update", msg)
 }
