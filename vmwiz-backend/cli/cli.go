b93d56524e652e33358216c95984554c1872cf08
Jonas Maier <jmaier@sos.ethz.ch>
2025-06-05 22:59:30 +0200

    use climate for good cli

--- vmwiz-backend/cli/cli.go
+++ vmwiz-backend/cli/cli.go
@@ -2,59 +2,27 @@ package cli
 
 import (
 	"fmt"
-	"os"
+
+	_ "embed"
 
 	"git.sos.ethz.ch/vsos/app.vsos.ethz.ch/vmwiz-backend/netcenter"
 	"git.sos.ethz.ch/vsos/app.vsos.ethz.ch/vmwiz-backend/startupcheck"
 	"git.sos.ethz.ch/vsos/app.vsos.ethz.ch/vmwiz-backend/storage"
+
+	"github.com/avamsi/climate"
 )
 
-type verb struct {
-	name string
-	desc string
-	fun  func()
-}
+type vw struct{}
 
-func check_health() {
+func (v *vw) Health() {
 	startupcheck.DoAllStartupChecks()
 }
 
-func list_requests() {
-	all := false
-
-	for i := 2; i < len(os.Args); i++ {
-		if os.Args[i] == "all" || os.Args[i] == "--all" {
-			all = true
-			continue
-		}
-
-		fmt.Printf("unknown flag `%s`\n", os.Args[i])
-		return
-	}
-
-	requests, err := storage.DB.GetAllVMRequests()
-
-	if err != nil {
-		fmt.Printf("failed to get VM requests: %v\n", err)
-		return
-	}
-
-	numPrintedReqs := 0
-
-	for _, req := range requests {
-		if !all && req.RequestStatus != storage.REQUEST_STATUS_PENDING {
-			continue
-		}
-		fmt.Printf("%s\n", req.ToString())
-		numPrintedReqs += 1
-	}
-
-	if numPrintedReqs == 0 {
-		fmt.Println("no requests to display.")
-	}
+type ip struct {
+	V *vw
 }
 
-func list_ips() {
+func (i *ip) List() {
 	ips, err := netcenter.GetFreeIPv4sInSubnet(netcenter.VM_SUBNET.V4net)
 	if err != nil {
 		fmt.Printf("failed to fetch free IPs: %v\n", err)
@@ -66,58 +34,42 @@ func list_ips() {
 	}
 }
 
-func help() {
-	for _, verb := range verbs {
-		fmt.Printf("%-16s %s\n", verb.name, verb.desc)
+type request struct {
+	V *vw
+}
+
+type listOptions struct {
+	All bool // also display accepted and rejected requests
+}
+
+func (r *request) List(opts *listOptions) {
+	requests, err := storage.DB.GetAllVMRequests()
+
+	if err != nil {
+		fmt.Printf("failed to get VM requests: %v\n", err)
+		return
+	}
+
+	numPrintedReqs := 0
+
+	for _, req := range requests {
+		if opts.All && req.RequestStatus != storage.REQUEST_STATUS_PENDING {
+			continue
+		}
+		fmt.Printf("%s\n", req.ToString())
+		numPrintedReqs += 1
+	}
+
+	if numPrintedReqs == 0 {
+		fmt.Println("no requests to display.")
 	}
 }
 
-var verbs []verb
-
-func initVerbs() {
-	verbs = []verb{
-		{
-			name: "help",
-			desc: "displays this help page",
-			fun:  help,
-		},
-		{
-			name: "check-health",
-			desc: "checks if all services are running correctly",
-			fun:  check_health,
-		},
-		{
-			name: "list-requests",
-			desc: "lists outstanding VM requests",
-			fun:  list_requests,
-		},
-		{
-			name: "list-ips",
-			desc: "list available IPv4s",
-			fun:  list_ips,
-		},
-	}
-}
+//go:generate go tool cligen md.cli
+//go:embed md.cli
+var md []byte
 
 func Main() {
-	initVerbs()
-
-	if len(os.Args) < 2 {
-		fmt.Println("you need to provide a verb to tell me what to do")
-		help()
-	} else {
-		found := false
-		for _, cmd := range verbs {
-			if cmd.name == os.Args[1] {
-				cmd.fun()
-				found = true
-				break
-			}
-		}
-
-		if !found {
-			fmt.Printf("unknown verb `%s`\n", os.Args[1])
-			help()
-		}
-	}
+	p := climate.Struct[vw](climate.Struct[ip](), climate.Struct[request]())
+	climate.RunAndExit(p, climate.WithMetadata(md))
 }
